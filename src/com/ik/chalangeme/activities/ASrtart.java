c6aff7211bf7741176a822348082ab8b7ccd187f
Ihor Karpachev <benchakalaka@gmail.com>
2014-07-23 17:14:05 +0100

    first commit

---
+++ src/com/ik/chalangeme/activities/ASrtart.java
@@ -0,0 +1,11 @@
+package com.ik.chalangeme.activities;
+
+import org.androidannotations.annotations.EActivity;
+
+import android.app.Activity;
+
+import com.ik.chalangeme.R;
+
+@EActivity ( R.layout.activity_srtart ) public class ASrtart extends Activity {
+
+}
